2c7192bcadf9155d96ce1866e7252db0ab4ed874
Kennedy Muinde <kennedymuinde@gmail.com>
2023-05-19 21:14:24 +0300

    Completed assignement

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES
+('Chris', 3, 'M', 'Brown', 'Aggressive', 1),
+  ('Mat', 5, 'M', 'Black', 'Gentle', 0),
+  ('Lily', 2, 'F', 'White', 'Playful', 1),
+  ('Ben', 4, 'M', 'Brown', 'Calm', 1),
+  ('Emma', 6, 'F', 'Black', 'Energetic', 0),
+  ('Jack', 1, 'M', 'Blonde', 'Curious', 1),
+  ('Sophia', 3, 'F', 'Brown', 'Friendly', 0),
+  (NULL, 2, 'M', 'Black', 'Shy', 1);
